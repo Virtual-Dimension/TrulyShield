0c59fc3aa08f94275e221d4109e31fc873c6ef3b
ArachnidaKing <3145170770@qq.com>
2019-11-12 17:43:58 +0800

    ☕ Update for list-page

--- TrulyLuoguShield.js
+++ TrulyLuoguShield.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		 真·洛谷屏蔽 Truly Luogu Shield
 // @namespace	 https://github.com/Virtual-Dimension/TrulyShield/blob/master/TrulyLuoguShield.js
-// @version	     1.0.2
+// @version	     1.1.2
 // @description  Shield information you don't want to see.
 // @author	     ArachnidaKing
 // @license      GPL-3.0
@@ -12,6 +12,9 @@
 'use strict';
 
 (() => {
+    let isCardElement = (element) => {
+        return element.className.search('am-comment') !== -1 || element.className.search('lg-table-bg0') !== -1;
+    };
     let mRequest = (medth, href, fn) => {
         let xmlHttp = new window.XMLHttpRequest();
         xmlHttp.open(medth, href, true);
@@ -21,14 +24,18 @@
     window.addEventListener('load', () => {
         let urlList = document.querySelectorAll("a[class=center]");
         for (const item of urlList) {
-            ((itemElement) => {
-                mRequest("GET", itemElement.href, res => {
-                    let prePos = res.search("JSON.parse\(.*?(?=\);)");
-                    let nxtPos = res.search("(?<=JSON.parse\().*?(?=\);)");
-                    res = window.eval(res.substr(prePos, nxtPos - prePos));
-                    if (res.currentData.user.userRelationship === 2) { itemElement.parentNode.parentNode.innerText = ['因内容作者为用户', itemElement.innerText, '，已屏蔽'].join(''); }
-                });
-            })(item);
+            let card = item;
+            while (card.id !== 'app' && (!card.className || !isCardElement(card))) card = card.parentElement;
+            if (card.id !== 'app') {
+                ((itemElement, cardElement) => {
+                    mRequest("GET", itemElement.href, res => {
+                        let prePos = res.search("JSON.parse\(.*?(?=\);)");
+                        let nxtPos = res.search("(?<=JSON.parse\().*?(?=\);)");
+                        res = window.eval(res.substr(prePos, nxtPos - prePos));
+                        if (res.currentData.user.userRelationship === 2) { cardElement.innerText = ['因内容作者为', res.currentData.user.name, '，已屏蔽'].join(''); }
+                    });
+                })(item, card);
+            }
         }
     });
 })();
